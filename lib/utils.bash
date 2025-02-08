e268791fa452b98c9c716180de68aa74f5b7a5d1
Eriks Zelenka <isindir@users.sf.net>
2025-02-08 20:08:46 +0000

    add initial logic for buf installation

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for buf.
 GH_REPO="https://github.com/bufbuild/buf"
 TOOL_NAME="buf"
 TOOL_TEST="buf --version"
@@ -31,7 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
 	# Change this function if buf has other means of determining installable versions.
 	list_github_tags
 }
@@ -41,11 +39,13 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for buf
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	echo "* Installing $TOOL_NAME release $version..."
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	for tool in buf protoc-gen-buf-breaking protoc-gen-buf-lint; do
+	  echo "  Installing $tool"
+	  url="${GH_REPO#https://}/cmd/${tool}@v${version}"
+	  GOBIN=$(dirname "$filename") go install $url
+	done
 }
 
 install_version() {
@@ -61,7 +61,7 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert buf executable exists.
+		# Assert bingo executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
